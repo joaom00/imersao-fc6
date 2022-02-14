76bfc70c8820bf021b9878d900f144a94675ac8c
João Pedro <joaopsilvamagalhaes@gmail.com>
2022-02-13 23:24:38 -0300

    nest api

---
+++ nestjs/src/tweets/tweets.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateTweetDto } from './dto/create-tweet.dto';
+import { UpdateTweetDto } from './dto/update-tweet.dto';
+import { Tweet, TweetDocument } from './schemas/tweet.schema';
+
+@Injectable()
+export class TweetsService {
+  constructor(
+    @InjectModel(Tweet.name)
+    private tweetModel: Model<TweetDocument>,
+  ) {}
+
+  create(createTweetDto: CreateTweetDto) {
+    return this.tweetModel.create(createTweetDto);
+  }
+
+  findAll(options?: { offset: number; limit: number }) {
+    const { offset = 0, limit = 50 } = options;
+
+    return this.tweetModel.find().skip(offset).limit(limit).exec();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} tweet`;
+  }
+
+  update(id: number, updateTweetDto: UpdateTweetDto) {
+    return `This action updates a #${id} tweet`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} tweet`;
+  }
+}
